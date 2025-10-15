81a0a4e676915ab4782ddbc0f0eb36b1b4a2f7b0
DerFlux <129894235+DerFlux@users.noreply.github.com>
2025-10-15 22:33:39 +0200

    Update get-theme.js

--- src/site/get-theme.js
+++ src/site/get-theme.js
@@ -5,13 +5,21 @@ const crypto = require("crypto");
 const {globSync} = require("glob");
 
 const themeCommentRegex = /\/\*[\s\S]*?\*\//g;
-    
-    const fs = require('fs');
-    const path = require('path');
-    const chosen = path.join(__dirname, '../styles/_theme.YOURHASH.css'); // your existing variable
-    const stable = path.join(__dirname, '../styles/theme.css');
-    fs.copyFileSync(chosen, stable);
-    console.log('Wrote stable theme.css');
+
+async function getTheme() {
+  let themeUrl = process.env.THEME;
+  if (themeUrl) {
+    //https://forum.obsidian.md/t/1-0-theme-migration-guide/42537
+    //Not all themes with no legacy mark have a theme.css file, so we need to check for it
+    try {
+      await axios.get(themeUrl);
+    } catch {
+      if (themeUrl.indexOf("theme.css") > -1) {
+        themeUrl = themeUrl.replace("theme.css", "obsidian.css");
+      } else if (themeUrl.indexOf("obsidian.css") > -1) {
+        themeUrl = themeUrl.replace("obsidian.css", "theme.css");
+      }
+    }
 
     const res = await axios.get(themeUrl);
     try {
